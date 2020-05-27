1576a42d44260bb61b64f9d1c420c7ef09e962b1
naomikgrant <33996698+naomikgrant@users.noreply.github.com>
2020-05-27 14:10:57 +0800

    Add files via upload

--- Processed/ProcessData.py
+++ Processed/ProcessData.py
@@ -3,30 +3,53 @@ import pickle
 import matplotlib
 import matplotlib.pyplot as plt
 
-# Loading saved model & model history (before adding filter)
-print("Loading model (before filter)...")
-model_bf = tf.keras.models.load_model('SisFall_Model_BF.h5')
-print("Loading model's history (before filter)...")
+# Loading saved model history (before adding filter)
+print("Loading MLP's model's history before and after filtering...")
 historyBF = pickle.load(open("HistoryBF.pickle", "rb"))
+historyAF = pickle.load(open("HistoryAF.pickle", "rb"))
 
-print("Plotting graphs...")
+print("Loading RNN's model's history before and after filtering...")
+historyBF_RNN = pickle.load(open("HistoryBF_RNN.pickle", "rb"))
+historyAF_RNN = pickle.load(open("HistoryAF_RNN.pickle", "rb"))
+
+print("Plotting graphs for MLP model...")
 # Plot training & validation accuracy values
 fig1 = plt.figure(1)
-plt.plot(historyBF['acc'])
-plt.plot(historyBF['val_acc'])
-plt.title('Model Accuracy Before Filter')
+plt.plot(historyBF['accuracy'])
+plt.plot(historyBF['val_accuracy'])
+plt.title('Model Accuracy Before Filter via MLP Model')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Training', 'Validation'], loc='center right')
-plt.savefig('SisFall_Accuracy(BF).png')
+plt.savefig('SisFall_MLP_Accuracy(BF).png')
 
 # Plot training & validation loss values
 plt.figure(2)
-plt.plot(historyBF['loss'])
-plt.plot(historyBF['val_loss'])
-plt.title('Model Loss Before Filter')
-plt.ylabel('Loss')
+plt.plot(historyAF['accuracy'])
+plt.plot(historyAF['val_accuracy'])
+plt.title('Model Accuracy After Filter via MLP Model')
+plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Training', 'Validation'], loc='center right')
-plt.savefig('SisFall_Loss(BF).png')
+plt.savefig('SisFall_MLP_Accuracy(AF).png')
 
+print("Plotting graphs for RNN model...")
+# Plot training & validation accuracy values
+fig1 = plt.figure(3)
+plt.plot(historyBF_RNN['accuracy'])
+plt.plot(historyBF_RNN['val_accuracy'])
+plt.title('Model Accuracy Before Filter via RNN Model')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Training', 'Validation'], loc='center right')
+plt.savefig('SisFall_RNN_Accuracy(BF).png')
+
+# Plot training & validation loss values
+plt.figure(4)
+plt.plot(historyAF_RNN['accuracy'])
+plt.plot(historyAF_RNN['val_accuracy'])
+plt.title('Model Accuracy After Filter via RNN Model')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Training', 'Validation'], loc='center right')
+plt.savefig('SisFall_RNN_Accuracy(AF).png')
